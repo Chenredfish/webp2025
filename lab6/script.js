3aee459a196f51bc8b5a232d544d912960ec0a7b
Chenredfish <75733853+Chenredfish@users.noreply.github.com>
2025-04-02 14:39:02 +0800

    Add files via upload

---
+++ lab6/script.js
@@ -0,0 +1,16 @@
+﻿var count = 1;
+function addfunction(){
+    var btn = document.createElement("button");
+    btn.innerHTML = `CLICK ME (${count})`;
+    btn.setAttribute("id", "btn_"+count++);
+    btn.setAttribute("class", "btn btn-outline-danger");
+    console.log(btn);
+    document.body.appendChild(btn);
+};
+
+function delfunction(){
+    var btn = document.getElementById("btn_"+--count);
+    console.log(btn);
+    document.body.removeChild(btn);
+
+}
