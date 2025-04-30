e182b606f5dc0595caea3581cc41e48d561ede0d
Chenredfish <75733853+Chenredfish@users.noreply.github.com>
2025-04-30 15:54:59 +0800

    Add files via upload

---
+++ lab12/src/App.js
@@ -0,0 +1,22 @@
+//import logo from './logo.svg';
+import './App.css';
+import React from "react";
+import MultiButton from './cgu_multiButton.js';
+import HelloCGU from './cgu_hello.js';
+
+
+function App() {
+  return (
+    <div className="App">
+      <div>
+        {HelloCGU()}
+      </div>
+      <div>
+        { MultiButton(10)}
+      </div>
+    </div>
+  );
+}
+
+
+export default App;
